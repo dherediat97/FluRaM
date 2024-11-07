fd2364249d4289573799499a2d113c6f8e17ca9b
dherediat <dherediat97@gmail.com>
2024-11-07 15:49:34 +0100

    feature: refactor widgets :wrench:

--- lib/presentation/screens/character_list/character_list.dart
+++ lib/presentation/screens/character_list/character_list.dart
@@ -1,19 +1,19 @@
-import 'package:fluram/presentation/screens/widgets/location_list_item.dart';
-import 'package:fluram/presentation/view_models/location_notifier.dart';
+import 'package:fluram/presentation/screens/character_list/widgets/character_list_item.dart';
+import 'package:fluram/presentation/view_models/character_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LocationList extends ConsumerStatefulWidget {
-  const LocationList({super.key});
+class CharacterList extends ConsumerStatefulWidget {
+  const CharacterList({super.key});
 
   @override
-  ConsumerState<LocationList> createState() => _LocationListState();
+  ConsumerState<CharacterList> createState() => _CharacterListState();
 }
 
-class _LocationListState extends ConsumerState<LocationList> {
+class _CharacterListState extends ConsumerState<CharacterList> {
   @override
   Widget build(BuildContext context) {
-    var provider = ref.watch(fetchAllLocationsProvider);
+    var provider = ref.watch(fetchAllCharactersProvider);
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -27,7 +27,7 @@ class _LocationListState extends ConsumerState<LocationList> {
                   physics: const NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
                   itemBuilder: (context, index) {
-                    return LocationListItem(location: data.results[index]);
+                    return CharacterListItem(character: data.results[index]);
                   },
                 )
               ],
